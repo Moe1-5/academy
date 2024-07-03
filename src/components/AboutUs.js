83c4e4b4d8423b42b426fd7854a3be8e572d25b8
mohammed <albadanimohammed7@gmail.com>
2024-07-03 23:27:22 +0800

    Update the repository with recent changes

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,30 +1,40 @@
 import react from 'react';
 import firstImage from '../images/first-image.avif';
 import secondImage from '../images/second-image.avif';
-
+import logo from '../images/logo.png'
 const AboutUs = () => {
     return (
-        <main>
-            <div className='cards-container'>
-                <div className='main-description'>
-                    <div className='text-container'>
-                        <h4>Our mission is to create a seamless digital learning environment that enhances student learning experiences and simplifies administrative tasks for educators. We aim to offer easy access to educational resources, foster student engagement through interactive video lectures, and streamline the enrollment process. Our goal is to contribute to a more effective educational environment, supporting academic success and administrative efficiency.</h4>
-                    </div>
-                    <hr className='line' />
-                    <img src={firstImage} alt='Friends Study' className='home-main-img' />
+        <div>
+            <nav className='home-nav'>
+                <img src={logo} alt='LOGO' className='home-img' />
+                <div className='home-nav-container'>
+                    <button className='home-btn'>Sign Up</button>
+                    <button className='home-btn'>Log In</button>
+                    <span className='home-span'><a href='./'>Home</a></span>
+                    <span className='home-span'><a href='./Contact-us'>Contact Us</a></span>
                 </div>
-            </div>
-
-            <div className='cards-container'>
-                <div className='main-description'>
-                    <img src={secondImage} alt='Friends Study' className='home-main-img second-main-img' />
-                    <hr className='line' />
-                    <div className='text-container second-text-container'>
-                        <h4>Our primary users are students, typically high school and university students aged 15-25. They are tech-savvy and rely on our platform for accessing educational materials, video lectures, and monitoring their academic progress. Educators, including teachers and lecturers aged 25-60, form our secondary audience. They require an efficient system to manage course materials, upload lectures, update syllabi, and monitor student engagement. Our platform supports educators with varying levels of technical proficiency.</h4>
+            </nav>
+            <main>
+                <div className='cards-container'>
+                    <div className='main-description'>
+                        <div className='text-container'>
+                            <h4>Our mission is to create a seamless digital learning environment that enhances student learning experiences and simplifies administrative tasks for educators. We aim to offer easy access to educational resources, foster student engagement through interactive video lectures, and streamline the enrollment process. Our goal is to contribute to a more effective educational environment, supporting academic success and administrative efficiency.</h4>
+                        </div>
+                        <hr className='line' />
+                        <img src={firstImage} alt='Friends Study' className='home-main-img' />
                     </div>
                 </div>
-            </div>
-        </main>
+                <div className='cards-container'>
+                    <div className='main-description'>
+                        <img src={secondImage} alt='Friends Study' className='home-main-img second-main-img' />
+                        <hr className='line' />
+                        <div className='text-container second-text-container'>
+                            <h4>Our primary users are students, typically high school and university students aged 15-25. They are tech-savvy and rely on our platform for accessing educational materials, video lectures, and monitoring their academic progress. Educators, including teachers and lecturers aged 25-60, form our secondary audience. They require an efficient system to manage course materials, upload lectures, update syllabi, and monitor student engagement. Our platform supports educators with varying levels of technical proficiency.</h4>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
     )
 }
 
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,16 +1,27 @@
 import react from 'react'
+import logo from '../images/logo.png'
 
 const ContactUs = () => {
     return (
-        <main className='contact-container'>
-            <div className='contact-card'>
-                <p>You can contact us through our Gmail : <span className='mail-text'>ExcellenceAcademy@gmail.com</span></p>
-                <div class="outer-circle">
-                    <div class="inner-circle"></div>
+        <div>
+            <nav className='home-nav'>
+                <img src={logo} alt='LOGO' className='home-img' />
+                <div className='home-nav-container'>
+                    <button className='home-btn'>Sign Up</button>
+                    <button className='home-btn'>Log In</button>
+                    <span className='home-span'><a href='./'>Home</a></span>
+                    <span className='home-span'><a href='./About-us'>About Us</a></span>
                 </div>
-
-            </div>
-        </main>
+            </nav>
+            <main className='contact-container'>
+                <div className='contact-card'>
+                    <p>You can contact us through our Gmail : <span className='mail-text'>ExcellenceAcademy@gmail.com</span></p>
+                    <div class="outer-circle">
+                        <div class="inner-circle"></div>
+                    </div>
+                </div>
+            </main>
+        </div>
     )
 }
 
